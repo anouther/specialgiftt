fa08eef6160d9a4a77b72cc1d2f3822962a3f744
anouther <155570107+anouther@users.noreply.github.com>
2024-12-10 22:08:25 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
       const c = setTimeout(() => {
         document.body.classList.remove("not-loaded");
 
-        const titles = ('I love you').split('');
+        const titles = ('Hi ebby').split('');
         const titleElement = document.getElementById('title');
         let index = 0;
 
