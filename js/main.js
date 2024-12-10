95d775b0ca0e9d51ace134d073a2d57b05071069
anouther <155570107+anouther@users.noreply.github.com>
2024-12-10 21:48:37 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "Each moment we've shared is truly a gift. You bring laughter, love, and comfort into my life. You're my inspiration, my anchor, my comfort, my sunshine, and my everything. You are my home, my love, my world.";
+            subtitle.innerText = "I bring flowers for you as a reward for your hard work today.";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
